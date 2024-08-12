a4eb09310f993989238423fda84ce89f4ac366e4
hemanialaparthi <alaparthi01@allegheny.edu>
2024-08-12 15:50:48 -0400

    edit: firebase.js

--- flashcards/firebase.js
+++ flashcards/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxWcExPQuuqFx8riMPwZ5WRn9jyQ5_pv0",
-  authDomain: "flashcards-c1c8a.firebaseapp.com",
-  projectId: "flashcards-c1c8a",
-  storageBucket: "flashcards-c1c8a.appspot.com",
-  messagingSenderId: "298468054023",
-  appId: "1:298468054023:web:7ef6f098c8bbaee67106a2"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
